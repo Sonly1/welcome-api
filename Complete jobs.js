6ac57627d07a86e83fe81eb42648a3f33639271c
Ramy K <kader.ramy@gmail.com>
2024-05-15 14:06:56 -0400

    Add files via upload

---
+++ Complete jobs.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const fs = require('fs');
+const readline = require('readline');
+
+// Define your base API endpoint
+const baseUrl = 'https://api.us-west-2.parsable.net/api/v3/teams/';
+
+// Define the teamId
+const teamId = '{{team_ID}}';
+
+// Define the headers for your request
+const headers = {
+  'accept': '*/*',
+  'parsable-custom-touchstone': 'test',
+  'Authorization': 'Bearer {{auth token}}' // Replace 'xxxxxx' with your token
+};
+
+// Path to your file
+const filePath = '/users/ramy.kader/Documents/Complete_jobs.csv';
+
+// Creating a readable stream and interface to read file line by line
+const readInterface = readline.createInterface({
+  input: fs.createReadStream(filePath),
+  output: process.stdout,
+  console: false
+});
+
+let index = 0;
+
+// Reading lines from the file
+readInterface.on('line', function(line) {
+  const jobId = line.split(',')[0].trim(); // This is the new part
+  const url = `${baseUrl}${teamId}/jobs/${jobId}?complete`;
+
+  axios.delete(url, { headers: headers })
+    .then(response => {
+      console.log(`Request ${index + 1} for team ID ${teamId} and job ID ${jobId} completed successfully.`);
+      index += 1;
+    })
+    .catch(error => {
+      console.error(`Request ${index + 1} for team ID ${teamId} and job ID ${jobId} failed with error:`, error.response ? error.response.data : error.message);
+      index += 1;
+    });
+});
+;
